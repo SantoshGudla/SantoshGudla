b66b20ccc911448b2ad0b1d5c3b23cc7ce9ea519
Gurubelli varun <gudlasantosh31@gmail.com>
2021-11-01 16:36:50 +0530

    sn ommit

--- src/app/companies/companies.component.ts
+++ src/app/companies/companies.component.ts
@@ -18,6 +18,7 @@ export class CompaniesComponent implements OnInit {
         this._userSvc.getData().subscribe(data => {
             this.mainListdata = data;
             this.modifydata();
+            
         });
     }
 
